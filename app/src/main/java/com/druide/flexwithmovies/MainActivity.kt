787f47a2573a1cc804cce32a2b1e20a2ff39f8cf
Djermoune Mehdi <mehdi.djermoune11@gmail.com>
2022-05-25 11:38:31 +0200

    Initial commit

---
+++ app/src/main/java/com/druide/flexwithmovies/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.druide.flexwithmovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
